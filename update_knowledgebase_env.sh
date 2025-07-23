d11d5f566c7765c40303db09e3e15061edba5c38
SimonKonstantinov <33290779+SimonKonstantinov@users.noreply.github.com>
2025-07-23 14:47:51 +0300

    Update update_knowledgebase_env.sh

--- update_knowledgebase_env.sh
+++ update_knowledgebase_env.sh
@@ -3,42 +3,30 @@
 # Директория, где находятся файлы .env
 BASE_DIR="knowledgebase"
 
-# Определите новые значения для переменных окружения здесь
-# Замените эти значения-заполнители на ваши реальные значения
-NEW_MINIO_ROOT_USER="new_minioadmin"
-NEW_MINIO_ROOT_PASSWORD="new_minio_password_789"
-NEW_DB_TYPE="postgres"
-NEW_DB_HOST="new_onlyoffice_postgresql"
-NEW_DB_PORT="5432"
-NEW_DB_NAME="new_dbname"
-NEW_DB_USER="new_dbuser"
-NEW_WOPI_ENABLED="true"
-NEW_AMQP_URI="amqp://new_guest:new_guest@new_onlyoffice_rabbitmq"
-NEW_JWT_ENABLED="false"
-NEW_ALLOW_PRIVATE_IP_ADDRESS="true"
-NEW_ALLOW_META_IP_ADDRESS="true"
-NEW_USE_UNAUTHORIZED_STORAGE="true"
+# Загрузка переменных из knowledgebase.env
+ENV_FILE="$BASE_DIR/knowledgebase.env"
+if [ ! -f "$ENV_FILE" ]; then
+    echo "Ошибка: Файл $ENV_FILE не найден"
+    exit 1
+fi
 
-# Функция для экранирования специальных символов для sed
-escape_sed() {
-    echo "$1" | sed -e 's/[\/&]/\\&/g'
-}
+source "$ENV_FILE"
 
 # Массив файлов и переменных для замены
 declare -A FILES=(
-    ["minio/minio_env.env"]="MINIO_ROOT_USER=$NEW_MINIO_ROOT_USER
-MINIO_ROOT_PASSWORD=$NEW_MINIO_ROOT_PASSWORD"
-    ["Docker-DocumentServer/onlyoffice_env.env"]="DB_TYPE=$NEW_DB_TYPE
-DB_HOST=$NEW_DB_HOST
-DB_PORT=$NEW_DB_PORT
-DB_NAME=$NEW_DB_NAME
-DB_USER=$NEW_DB_USER
-WOPI_ENABLED=$NEW_WOPI_ENABLED
-AMQP_URI=$NEW_AMQP_URI
-JWT_ENABLED=$NEW_JWT_ENABLED
-ALLOW_PRIVATE_IP_ADDRESS=$NEW_ALLOW_PRIVATE_IP_ADDRESS
-ALLOW_META_IP_ADDRESS=$NEW_ALLOW_META_IP_ADDRESS
-USE_UNAUTHORIZED_STORAGE=$NEW_USE_UNAUTHORIZED_STORAGE"
+    ["minio/minio_env.env"]="MINIO_ROOT_USER=$MINIO_ROOT_USER
+MINIO_ROOT_PASSWORD=$MINIO_ROOT_PASSWORD"
+    ["Docker-DocumentServer/onlyoffice_env.env"]="DB_TYPE=$DB_TYPE
+DB_HOST=$DB_HOST
+DB_PORT=$DB_PORT
+DB_NAME=$DB_NAME
+DB_USER=$DB_USER
+WOPI_ENABLED=$WOPI_ENABLED
+AMQP_URI=$AMQP_URI
+JWT_ENABLED=$JWT_ENABLED
+ALLOW_PRIVATE_IP_ADDRESS=$ALLOW_PRIVATE_IP_ADDRESS
+ALLOW_META_IP_ADDRESS=$ALLOW_META_IP_ADDRESS
+USE_UNAUTHORIZED_STORAGE=$USE_UNAUTHORIZED_STORAGE"
 )
 
 # Проверка существования базовой директории
@@ -46,6 +34,11 @@ if [ ! -d "$BASE_DIR" ]; then
     echo "Ошибка: Директория $BASE_DIR не существует"
     exit 1
 fi
+
+# Функция для экранирования специальных символов для sed
+escape_sed() {
+    echo "$1" | sed -e 's/[\/&]/\\&/g'
+}
 
 # Обработка каждого файла
 for file in "${!FILES[@]}"; do
